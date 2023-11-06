345157ad4f150540a2202436cd320744e3cb98fd
alfredo <alfloz01@ucm.es>
2023-11-06 17:24:19 +0100

    Try to correct vercel

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
-import './app.css';
 
 export function App() {
   const [count, setCount] = useState(0);
