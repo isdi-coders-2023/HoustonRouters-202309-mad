9b23a3bfef52a4bc5ff46d901ab16cd22bd4d637
alfredo <alfloz01@ucm.es>
2023-11-06 18:20:32 +0100

    Create loading page and test it

---
+++ src/components/app/App.tsx
@@ -0,0 +1,8 @@
+// import { AppRoutes } from '../router/app.routes';
+
+export function App() {
+  return;
+  // <AppRoutes>
+  return <></>;
+  /* </AppRoutes> */
+}
---
+++ src/components/loading/loading.tsx
@@ -0,0 +1,8 @@
+export function Loading(): React.ReactElement {
+  return (
+    <div>
+      <img src="./portal-rick-and-morty.gif" alt="Logo loading page"></img>
+      <span>Loading</span>
+    </div>
+  );
+}
