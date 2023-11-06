2314eae2bb8f40d83a4f2c19f0bd1643182d3a56
abelnhm <abelnhm92@gmail.com>
2023-11-06 18:24:39 +0100

    Add header and footer components

---
+++ src/components/app/App.tsx
@@ -0,0 +1,13 @@
+import { Footer } from '../footer/footer';
+import { Header } from '../header/header';
+
+export function App() {
+  return (
+    <>
+      <Header></Header>
+      <Footer></Footer>
+    </>
+  );
+}
+
+export default App;
