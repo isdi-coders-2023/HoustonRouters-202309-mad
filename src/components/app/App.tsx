2bdb7b31ce1bcf11eedc40d0967547b30e071cce
abelnhm <abelnhm92@gmail.com>
2023-11-06 21:46:50 +0100

    Add components to app

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,12 +1,16 @@
+import { Actions } from '../actions/actions';
 import { Display } from '../display/display';
 import { Footer } from '../footer/footer';
 import { Header } from '../header/header';
+import { List } from '../list/list';
 
 export function App() {
   return (
     <>
       <Header></Header>
       <Display></Display>
+      <Actions></Actions>
+      <List></List>
       <Footer></Footer>
     </>
   );
