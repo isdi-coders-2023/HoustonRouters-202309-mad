d87724a3f910b493e3411aba592d83b8ec9587aa
abelnhm <abelnhm92@gmail.com>
2023-11-07 10:48:12 +0100

    Add test to component buttonDetails

---
+++ src/components/button-details/button.details.test.tsx
@@ -0,0 +1,16 @@
+import { screen, render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { ButtonDetails } from './button.details';
+
+describe('Given ButtonDetails component', () => {
+  describe('When we instantiate', () => {
+    beforeEach(() => {
+      render(<ButtonDetails></ButtonDetails>);
+    });
+
+    test('Then it should be in the document', () => {
+      const element = screen.getByRole('button');
+      expect(element).toBeInTheDocument();
+    });
+  });
+});
