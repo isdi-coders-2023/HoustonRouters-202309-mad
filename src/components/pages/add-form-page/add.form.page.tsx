c9bfeaac6688810444f54691d960e622f94690a8
abelnhm <abelnhm92@gmail.com>
2023-11-07 13:46:29 +0100

    Add page to add form

---
+++ src/components/pages/add-form-page/add.form.page.tsx
@@ -0,0 +1,11 @@
+import { ButtonBack } from '../../button-back/button.back';
+import { FormCharacter } from '../../form/form';
+
+export default function AddFormPage() {
+  return (
+    <>
+      <FormCharacter></FormCharacter>
+      <ButtonBack></ButtonBack>
+    </>
+  );
+}
