b597773e9eb8a2aadf9bdc5197bb28cd6641b882
alfredo <alfloz01@ucm.es>
2023-11-07 18:14:10 +0100

    Correct list

--- src/components/pages/home/home.tsx
+++ src/components/pages/home/home.tsx
@@ -1,6 +1,5 @@
 import { Actions } from '../../actions/actions';
 import { Display } from '../../display/display';
-import { List } from '../../list/list';
 
 export default function HomePage() {
   return (
