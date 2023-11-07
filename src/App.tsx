ab1a5d5e1e8f834b4fb73fe7650e32c66c60d255
Franz <fernandofranquiz@gmail.com>
2023-11-07 09:29:22 +0100

    Deleting app file in the wrong place

--- src/App.tsx
+++
@@ -1,8 +0,0 @@
-import './components/details/detail.scss';
-import './components/button-back/button.back.scss';
-import { Detail } from './components/details/detail';
-function App() {
-  return <Detail></Detail>;
-}
-
-export default App;
