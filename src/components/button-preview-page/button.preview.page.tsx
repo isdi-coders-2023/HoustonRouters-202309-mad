7e48ff87b4a63eff7fc524531c95c771c607291f
Franz <fernandofranquiz@gmail.com>
2023-11-07 21:46:33 +0100

    Add funtions of buttons next and previous page

--- src/components/button-preview-page/button.preview.page.tsx
+++ src/components/button-preview-page/button.preview.page.tsx
@@ -1,6 +1,13 @@
+import { AppContext } from '../context/app.contest';
+import { useContext } from 'react';
+
 export function ButtonPreviewPage() {
+  const {
+    characterTools: { handlePrevious },
+  } = useContext(AppContext);
+
   return (
-    <button className="button-preview-page">
+    <button className="button-preview-page" onClick={handlePrevious}>
       <i className="fa-solid fa-arrow-left"></i> Back
     </button>
   );
--- src/components/pages/home/home.tsx
+++ src/components/pages/home/home.tsx
@@ -1,4 +1,5 @@
 import { Actions } from '../../actions/actions';
+import { ButtonNextPage } from '../../button-next-page/button.next.page';
 import { Display } from '../../display/display';
 
 export default function HomePage() {
