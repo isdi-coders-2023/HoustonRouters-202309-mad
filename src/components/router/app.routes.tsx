bcf674a4e409acc472b50f3f376e0180574af684
abelnhm <abelnhm92@gmail.com>
2023-11-07 15:45:35 +0100

    Fix router details

--- src/components/router/app.routes.tsx
+++ src/components/router/app.routes.tsx
@@ -1,11 +1,11 @@
 import { Suspense, lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { Detail } from '../details/detail';
 
 const HomePage = lazy(() => import('../pages/home/home'));
 const ErroPage = lazy(() => import('../pages/error-page/error.page'));
 const MyUniversePage = lazy(() => import('../pages/my-universe/my.universe'));
 const AddFormPage = lazy(() => import('../pages/add-form-page/add.form.page'));
+const DetailPage = lazy(() => import('../pages/details-page/detail.page'));
 
 export function AppRoutes() {
   return (
@@ -18,7 +18,7 @@ export function AppRoutes() {
           element={<MyUniversePage></MyUniversePage>}
         ></Route>
         <Route path="/add" element={<AddFormPage></AddFormPage>}></Route>
-        <Route path="/details" element={<Detail></Detail>}></Route>
+        <Route path="/details" element={<DetailPage></DetailPage>}></Route>
         <Route path="*" element={<ErroPage></ErroPage>}></Route>
       </Routes>
     </Suspense>
