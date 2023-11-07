d3c1d20154998a5ebc2e6eb170c2c845e34d5e6c
alfredo <alfloz01@ucm.es>
2023-11-07 14:28:39 +0100

    Correct sonar error

--- src/components/card/card.test.tsx
+++ src/components/card/card.test.tsx
@@ -5,7 +5,34 @@ import { Card } from './card';
 describe('Given Card component', () => {
   describe('When we instantiate', () => {
     beforeEach(() => {
-      render(<Card></Card>);
+      render(
+        <Card
+          character={{
+            id: 0,
+            name: '',
+            status: 'Alive',
+            species: '',
+            gender: 'Unknown',
+            origin: {
+              name: '',
+              url: '',
+            },
+            location: {
+              id: 0,
+              name: '',
+              type: '',
+              dimension: '',
+              residents: [],
+              url: '',
+              created: '',
+            },
+            image: '',
+            episode: [],
+            url: '',
+            created: '',
+          }}
+        ></Card>
+      );
     });
 
     test('Then it should be in the document', () => {
