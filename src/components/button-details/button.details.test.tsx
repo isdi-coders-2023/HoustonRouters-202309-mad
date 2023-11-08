072944f38ece5ade40691c7271ed1089d9a8face
alfredo <alfloz01@ucm.es>
2023-11-08 19:42:50 +0100

    Create details button test

--- src/components/button-details/button.details.test.tsx
+++ src/components/button-details/button.details.test.tsx
@@ -1,15 +1,20 @@
 import { screen, render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { ButtonDetails } from './button.details';
+import { MemoryRouter } from 'react-router-dom';
 
 describe('Given ButtonDetails component', () => {
   describe('When we instantiate', () => {
     beforeEach(() => {
-      render(<ButtonDetails idCharacter={''}></ButtonDetails>);
+      render(
+        <MemoryRouter initialEntries={['/details/123']}>
+          <ButtonDetails idCharacter={''}></ButtonDetails>;
+        </MemoryRouter>
+      );
     });
 
     test('Then it should be in the document', () => {
-      const element = screen.getByRole('button');
+      const element = screen.getByRole('link');
       expect(element).toBeInTheDocument();
     });
   });
--- src/components/button-next-page/button.next.page.test.tsx
+++ src/components/button-next-page/button.next.page.test.tsx
@@ -1,11 +1,19 @@
 import { screen, render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { ButtonNextPage } from './button.next.page';
+import { Route } from 'react-router-dom';
+import { AppContext } from '../context/app.contest';
 
 describe('Given ButtonNextPage component', () => {
   describe('When we instantiate', () => {
     beforeEach(() => {
-      render(<ButtonNextPage></ButtonNextPage>);
+      render(
+      <AppContext></AppContext>
+        <Route>
+          <ButtonNextPage></ButtonNextPage>
+        </Route>
+        
+      );
     });
 
     test('Then it should be in the document', () => {
