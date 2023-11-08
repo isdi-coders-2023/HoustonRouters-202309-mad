a7fb6092dcc6dd5696d9fbfc05c3d886674850df
alfredo <alfloz01@ucm.es>
2023-11-08 11:10:16 +0100

    Create Mock test

--- src/components/list/list.test.tsx
+++ src/components/list/list.test.tsx
@@ -1,18 +1,27 @@
-// import '@testing-library/jest-dom';
-// import { List } from './list';
-// import { render } from '@testing-library/react';
-// import { ContextStructure } from '../context/app.contest';
+import '@testing-library/jest-dom';
+import { List } from './list';
+import { render } from '@testing-library/react';
+import { AppContext, ContextStructure } from '../context/app.contest';
+import { Character } from '../../models/character';
 
-// const mockContext: ContextStructure = {
-//   characterTools: {
+const mockContext: ContextStructure = {
+  characterTools: {
+    characters: [{ Nombre: 'Rick' }] as unknown as Character[],
+    loadCharacters: jest
+      .fn()
+      .mockResolvedValue([{ Nombre: 'Bart' } as unknown as Character]),
+  },
+} as unknown as ContextStructure;
 
-// };
-
-// describe('given the List component', () => {
-//   describe('wen we initiate ', () => {
-//     beforeEach(() => {
-//       render(<List></List>);
-//     });
-//     test('is should have cards in him');
-//   });
-// });
+describe('given the List component', () => {
+  describe('wen we initiate ', () => {
+    beforeEach(() => {
+      render(
+        <AppContext.Provider value={mockContext}>
+          <List></List>
+        </AppContext.Provider>
+      );
+    });
+    test('is should have cards in him');
+  });
+});
