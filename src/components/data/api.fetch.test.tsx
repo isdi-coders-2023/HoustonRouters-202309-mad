b8bde4157d15ecdd68ab14c6d33ef06c2bdef5c1
alfredo <alfloz01@ucm.es>
2023-11-08 10:17:28 +0100

    Restore test app and correct back button test

--- src/components/data/api.fetch.test.tsx
+++ src/components/data/api.fetch.test.tsx
@@ -1,4 +1,4 @@
-import { Character } from '../models/character';
+import { Character } from '../../models/character';
 import { ApiCharacters } from './api.fetch';
 
 describe('Given ApiRepo class', () => {
