3994b4d5031ee54d9ac8491035771fd5bc1c3d31
abelnhm <abelnhm92@gmail.com>
2023-11-10 12:15:20 +0100

    Add test to filters

--- src/components/action-filter-gender/action.filter.gender.test.tsx
+++ src/components/action-filter-gender/action.filter.gender.test.tsx
@@ -1,11 +1,22 @@
 import { screen, render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { ActionFilterGender } from './action.filter.gender';
+import { AppContext, ContextStructure } from '../context/app.contest';
+
+const mockContext: ContextStructure = {
+  characterTools: {
+    handleFilterGender: jest.fn(),
+  },
+} as unknown as ContextStructure;
 
 describe('Given ActionFilterStatus component', () => {
   describe('When we instantiate', () => {
     beforeEach(() => {
-      render(<ActionFilterGender></ActionFilterGender>);
+      render(
+        <AppContext.Provider value={mockContext}>
+          <ActionFilterGender></ActionFilterGender>
+        </AppContext.Provider>
+      );
     });
 
     test('Then it should be in the document', () => {
