d76d4dd34ea49d51cc99fe58d2b9c571115bf561
abelnhm <abelnhm92@gmail.com>
2023-11-10 12:01:43 +0100

    Add test of actions

--- src/components/actions/actions.test.tsx
+++ src/components/actions/actions.test.tsx
@@ -2,6 +2,9 @@ import { screen, render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { Actions } from './actions';
 
+jest.mock('../action-filter-gender/action.filter.gender');
+jest.mock('../action-filter-status/action.filter.status');
+
 describe('Given Actions component', () => {
   describe('When we instantiate', () => {
     beforeEach(() => {
