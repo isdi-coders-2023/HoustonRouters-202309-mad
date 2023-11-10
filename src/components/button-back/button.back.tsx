89af80cecb7a39690de843b886ea9c57c6eae5b1
abelnhm <abelnhm92@gmail.com>
2023-11-10 13:07:27 +0100

    Update link to back and test

--- src/components/button-back/button.back.tsx
+++ src/components/button-back/button.back.tsx
@@ -1,8 +1,11 @@
+import { Link } from 'react-router-dom';
 import './button.back.scss';
 export function ButtonBack() {
   return (
     <div className="button-container">
-      <button className="button-back">Back</button>
+      <Link to="/" className="button-back">
+        Back
+      </Link>
     </div>
   );
 }
