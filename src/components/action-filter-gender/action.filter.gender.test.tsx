fe9650ab01ae2ae0c4da646498c29923ea5025ae
Franz <fernandofranquiz@gmail.com>
2023-11-11 17:47:21 +0100

    Fix of filters components test

--- src/components/action-filter-gender/action.filter.gender.test.tsx
+++ src/components/action-filter-gender/action.filter.gender.test.tsx
@@ -2,10 +2,12 @@ import { screen, render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { ActionFilterGender } from './action.filter.gender';
 import { AppContext, ContextStructure } from '../context/app.contest';
+import userEvent from '@testing-library/user-event';
 
 const mockContext: ContextStructure = {
   characterTools: {
     handleFilterGender: jest.fn(),
+    loadCharacters: jest.fn(),
   },
 } as unknown as ContextStructure;
 
@@ -22,6 +24,12 @@ describe('Given ActionFilterStatus component', () => {
     test('Then it should be in the document', () => {
       const element = screen.getByRole('combobox');
       expect(element).toBeInTheDocument();
+    });
+
+    test('Then the handleFilterSelectStatus should be executed', async () => {
+      const element = screen.getByRole('combobox');
+      await userEvent.selectOptions(element, 'Female');
+      expect(mockContext.characterTools.handleFilterGender).toHaveBeenCalled();
     });
   });
 });
--- src/components/action-filter-status/action.filter.status.test.tsx
+++ src/components/action-filter-status/action.filter.status.test.tsx
@@ -2,10 +2,12 @@ import { screen, render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { ActionFilterStatus } from './action.filter.status';
 import { AppContext, ContextStructure } from '../context/app.contest';
+import userEvent from '@testing-library/user-event';
 
 const mockContext: ContextStructure = {
   characterTools: {
     handleFilterStatus: jest.fn(),
+    loadCharacters: jest.fn(),
   },
 } as unknown as ContextStructure;
 
@@ -22,6 +24,12 @@ describe('Given ActionFilterStatus component', () => {
     test('Then it should be in the document', () => {
       const element = screen.getByRole('combobox');
       expect(element).toBeInTheDocument();
+    });
+
+    test('Then the handleFilterSelectStatus should be executed', async () => {
+      const element = screen.getByRole('combobox');
+      await userEvent.selectOptions(element, 'alive');
+      expect(mockContext.characterTools.handleFilterStatus).toHaveBeenCalled();
     });
   });
 });
