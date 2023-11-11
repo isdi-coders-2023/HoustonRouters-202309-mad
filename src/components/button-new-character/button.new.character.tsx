6f5bd892c860390238d019c80e94579dff8d865c
abelnhm <abelnhm92@gmail.com>
2023-11-11 14:56:00 +0100

    Update new button create character

--- src/components/button-new-character/button.new.character.tsx
+++ src/components/button-new-character/button.new.character.tsx
@@ -1,10 +1,12 @@
+import { Link } from 'react-router-dom';
 import './button.new.character.scss';
 
 export function ButtonNewCharacter() {
   return (
-    <button className="button-new-character">
-      {' '}
-      <i className="fa-solid fa-plus"></i> Create new character{' '}
-    </button>
+    <section className="action-new-character">
+      <Link to="/add" className="link-new-character">
+        <i className="fa-solid fa-plus"></i> Create new character
+      </Link>
+    </section>
   );
 }
