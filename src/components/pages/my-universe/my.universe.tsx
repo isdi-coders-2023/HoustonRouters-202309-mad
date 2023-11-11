d88895ba8ddd86b46fc016069c17ac6696fbf011
abelnhm <abelnhm92@gmail.com>
2023-11-11 16:05:05 +0100

    Fix import component list

--- src/components/pages/my-universe/my.universe.tsx
+++ src/components/pages/my-universe/my.universe.tsx
@@ -1,6 +1,7 @@
 import { ActionsList } from '../../actions-list/actions.list';
 import { ButtonNewCharacter } from '../../button-new-character/button.new.character';
 import { Display } from '../../display/display';
+import { List } from '../../list/list';
 
 export default function MyUniversePage() {
   return (
