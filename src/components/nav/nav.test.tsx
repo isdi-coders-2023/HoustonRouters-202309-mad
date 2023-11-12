36d518ff321686577b6ce6bbebf7e3f8534ffaa0
Franz <fernandofranquiz@gmail.com>
2023-11-12 19:58:18 +0100

    Fix test of nav component

--- src/components/nav/nav.test.tsx
+++ src/components/nav/nav.test.tsx
@@ -2,6 +2,7 @@ import { screen, render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { MemoryRouter as Router } from 'react-router-dom';
 import { Nav } from './nav';
+import userEvent from '@testing-library/user-event';
 
 describe('Given Header component', () => {
   describe('When we instantiate', () => {
@@ -14,8 +15,23 @@ describe('Given Header component', () => {
     });
 
     test('Then it should be in the document', () => {
-      const element = screen.getByRole('list');
+      const element = screen.getByRole('menu-desktop');
       expect(element).toBeInTheDocument();
+    });
+
+    test('Then it should be in the document', () => {
+      const element = screen.getByRole('menu-mobile');
+      expect(element).toBeInTheDocument();
+    });
+
+    test('Then it should be in the document', async () => {
+      const element = screen.getByRole('link-home');
+      await userEvent.click(element);
+    });
+
+    test('Then it should be in the document', async () => {
+      const element = screen.getByRole('i');
+      await userEvent.click(element);
     });
   });
 });
